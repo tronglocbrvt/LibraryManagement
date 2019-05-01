a5177a69e1f97e20f7e3d625fee4347a4e6ce74e
Sgray <phongnguyen6120@gmail.com>
2019-05-01 19:57:33 +0700

    Update commonFunction.h

--- LibraryManagement/commonFunction.h
+++ LibraryManagement/commonFunction.h
@@ -9,6 +9,12 @@ int numDaysOfMonth(int Month, int Year);
 
 bool isPossibleDay(int Day, int Month, int Year);
 
+int plusOneIntoAChar(char &numch);
+bool plusOneIntoAString(char* numStr);
+
+Day getExpiredDay(Day orginDay);
+Day getToday();
+
 bool isNumber(char c);
 
 void textBgColor(int colorText, int colorBG);
