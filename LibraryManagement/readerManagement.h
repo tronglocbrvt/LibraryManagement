7b21ae6a4cc2ce785fe9ce6819e5371a87df4247
Sgray <phongnguyen6120@gmail.com>
2019-05-01 19:53:01 +0700

    Create readerManagement.h

---
+++ LibraryManagement/readerManagement.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include "commonFunction.h"
+#include "LibraryManagement.h"
+
+
+void printfReader(Readers reader);
+
+Readers setReaderInf(char *readerID);
+
