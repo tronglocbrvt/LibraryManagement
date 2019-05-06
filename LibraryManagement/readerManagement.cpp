4f4d10b0316058647064ae57fb1147816b8d9f67
Sgray <phongnguyen6120@gmail.com>
2019-05-06 12:17:22 +0700

    Update readerManagement.cpp

--- LibraryManagement/readerManagement.cpp
+++ LibraryManagement/readerManagement.cpp
@@ -1,6 +1,34 @@
 #include "readerManagement.h"
 
+char *getNationalID()
+{
+	int flag = 0;
+	char *idNa = new char();
+	do
+	{
+		printf("Nhap CMND (9 so hoac 12 so): ");
+		scanf("%s", idNa);
+		int temp = getchar();
+		
+		if (strlen(idNa) != 9 && strlen(idNa) != 12)
+		{
+			printf("CMND khong hop le. Vui long nhap lai.\n");
+			continue;
+		}
 
+		flag = 1; 
+		for (int i = 0; i < strlen(idNa); i++)
+		{
+			if (!isNumber(idNa[i]))
+			{
+				flag = 0;
+				printf("CMND khong hop le. Vui long nhap lai.\n");
+				break;
+			}
+		}
+	} while (flag == 0);
+	return idNa;
+}
 void printfReader(Readers reader){
 
 	printf("Ma doc gia:\t\t%s\n", reader.ID);
@@ -18,41 +46,16 @@ void printfReader(Readers reader){
 Readers setReaderInf(char *readerID){
 	Readers reader;
 	int temp;
- 	// them ma doc gia
+ 	// thêm mã đọc giả -- tự động
 	strcpy(reader.ID, readerID);
-	// them ho va ten
+	// thêm họ và tên
 	printf("Nhap Ho va Ten: ");
 	gets(reader.Fullname);
 
-	do
-	{
-		printf("Nhap ngay sinh: ");
-		scanf("%d", &reader.Birthday.Date);
-		temp = getchar();
+	printf("Nhap ngay thang nam sinh:\n");
+	reader.Birthday = getDayFrmUser();
 
-		printf("Nhap thang sinh: ");
-		scanf("%d", &reader.Birthday.Month);
-		temp = getchar();
-
-		printf("Nhap nam sinh: ");
-		scanf("%d", &reader.Birthday.Year);
-		temp = getchar();
-
-		if (!isPossibleDay(reader.Birthday.Date, reader.Birthday.Month, reader.Birthday.Year))
-			printf("Ngay thang nam sinh khong hop le vui long nhap lai.\n");
-		else break;
-	} while (1);
-
-	do
-	{
-		printf("Nhap CMND (9 so hoac 12 so): ");
-		scanf("%s", &reader.NationID);
-		temp = getchar();
-
-		if (strlen(reader.NationID) != 9 && strlen(reader.NationID) != 12)
-			printf("CMND khong hop le. Vui long nhap lai.\n");
-		else break;
-	} while (1);
+	strcpy(&reader.NationID, getNationalID());
 
 	printf("Nhap dia chi: ");
 	gets(reader.Address);
@@ -71,11 +74,56 @@ Readers setReaderInf(char *readerID){
 	printf("Nhap dia chi email / thu dien tu: ");
 	gets(reader.Email);
 	
-	// ngay lap the
+	// ngày lập thẻ -- lấy ngày tạo tài khoản : hôm nay
 	reader.creatCard = getToday();
-	// ngay het han tu co ham
+	// ngày hết hạn là ngày tối đa được lập của thẻ, ngày tối đa là một hằng số
 	reader.expireCard = getExpiredDay(reader.creatCard);
-
 	//
 	return reader;
 }
+
+bool editReaderInf(Readers *reader){
+
+	switch(getNumberPressKey(printfEditReaderInf())){
+		case 1:
+			printf("Nhap Ho va Ten: ");
+			gets(reader.Fullname);
+			break;
+		case 2:
+			printf("Nhap moi CMND de sua:\n");
+			strcpy(reader->NationID, getNationalID);
+			break;
+		case 3:
+			printf("Nhap ngay thang nam sinh:\n");
+			reader.Birthday = getDayFrmUser();
+			break;
+		case 4:
+			do
+			{
+				printf("Nhap gioi tinh (Nam nhap 1; Nu nhap 0): ");
+				scanf("%d", &reader.Sex);
+				temp = getchar();
+
+				if (reader.Sex != 0 && reader.Sex != 1)
+					printf("Vui long nhap lai. Nam nhap 1, Nu nhap 0.\n");
+				else break;
+			} while (1);
+			break;
+		case 5:
+			printf("Nhap dia chi email / thu dien tu: ");
+			gets(reader.Email);
+			break;
+		case 6:
+			printf("Nhap dia chi: ");
+			gets(reader.Address);
+			break;
+		case 7:
+			reader.expireCard = getExpiredDay(getToday());
+			break;
+
+	}
+	return true;
+}
+
+
+
