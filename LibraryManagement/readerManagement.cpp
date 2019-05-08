4b9c237007dbbbce9cea984b0ce483bc026acca6
Sgray <phongnguyen6120@gmail.com>
2019-05-08 09:57:06 +0700

    Update readerManagement.cpp

--- LibraryManagement/readerManagement.cpp
+++ LibraryManagement/readerManagement.cpp
@@ -34,52 +34,94 @@ char *getNationalID()
 void printfReader(Readers reader){
 
 	printf("Ma doc gia:\t\t%s\n", reader.ID);
+
 	printf("Ho ten day du:\t\t%s\n", reader.Fullname);
+
 	printf("CMND:\t\t%s\n", reader.NationID);
-	printf("Ngay sinh:\t\t%d/ %d / %d\n", reader.Birthday.Date, reader.Birthday.Month, reader.Birthday.Year);
+
+	printf("Ngay sinh:\t\t");
+	printfDay(reader.Birthday);
+
 	printf("Gioi tinh:\t\t%s\n", (reader.Sex == 0 ? (char *)"Nu" : (char *)"Nam"));
+
 	printf("Thu dien tu:\t\t%s\n", reader.Email);
+
 	printf("Dia chi:\t\t%s\n", reader.Address);
-	printf("Ngay lap the:\t\t%d/ %d / %d\n", reader.creatCard.Date, reader.creatCard.Month, reader.creatCard.Year);
-	printf("Ngay het han:\t\t%d/ %d / %d\n", reader.expireCard.Date, reader.expireCard.Month, reader.expireCard.Year);
+
+	printf("Ngay lap the:\t\t");
+	printfDay(reader.creatCard);
+
+	printf("Ngay het han:\t\t%");
+	printfDay(reader.expireCard);
 
 }
 
-Readers setReaderInf(char *readerID){
-	Readers reader;
+bool printfAllReader(FILE *fileReader){	// đọc toàn bộ thông tin đọc giả từ file và in ra -> không đưa vào link list
+	Readers *reader = new Readers();
+	if (fileReader == NULL || reader == NULL)
+	{
+		return 0;
+	}
+	
+	while (fread(reader, sizeof(reader), 1 ,f) != NULL){
+		printfReader(*reader);
+	}
+	
+	fseek(fileReader, 0, SEEK_SET);
+	return 1;
+}
+
+bool printfAllReader(const LLNodeReader *ls){ // int ra thông tin đọc giả từ Link List khi đã có Link List
+	NodeReader *pNow = new NodeReader();
+	if (ls == NULL || pNow == NULL)
+	{
+		return 0;
+	}
+	pNow = ls->pHead;
+
+	while(pNow != NULL){
+		printfReader(*pNow->reader);
+		pNow = pNow->pNext;
+	}
+
+	return 1;
+}
+
+Readers *setReaderInf(char *readerID){
+	Readers *reader;
 	int temp;
  	// thêm mã đọc giả -- tự động
-	strcpy(reader.ID, readerID);
+	strcpy(reader->ID, readerID);
 	// thêm họ và tên
 	printf("Nhap Ho va Ten: ");
-	gets(reader.Fullname);
+	gets(reader->Fullname);
 
 	printf("Nhap ngay thang nam sinh:\n");
-	reader.Birthday = getDayFrmUser();
+	*reader->Birthday = getDayFrmUser();
 
-	strcpy(reader.NationID, getNationalID());
+	strcpy(reader->NationID, getNationalID());
 
 	printf("Nhap dia chi: ");
-	gets(reader.Address);
+	gets(reader->Address);
 
 	do
 	{
 		printf("Nhap gioi tinh (Nam nhap 1; Nu nhap 0): ");
-		scanf("%d", &reader.Sex);
+		scanf("%d", &reader->Sex);
 		temp = getchar();
 
-		if (reader.Sex != 0 && reader.Sex != 1)
+		if (reader->Sex != 0 && reader->Sex != 1)
 			printf("Vui long nhap lai. Nam nhap 1, Nu nhap 0.\n");
 		else break;
 	} while (1);
 
 	printf("Nhap dia chi email / thu dien tu: ");
-	gets(reader.Email);
+	gets(reader->Email);
 	
 	// ngày lập thẻ -- lấy ngày tạo tài khoản : hôm nay
-	reader.creatCard = getToday();
+	reader->creatCard = getToday();
 	// ngày hết hạn là ngày tối đa được lập của thẻ, ngày tối đa là một hằng số
-	reader.expireCard = getExpiredDay(reader.creatCard);
+	reader->expireCard = getExpiredDay(reader->creatCard);
 	//
 	return reader;
 }
