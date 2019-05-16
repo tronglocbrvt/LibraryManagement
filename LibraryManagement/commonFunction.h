11b3148fbfeae19d79c4223a2dd21acfb7611f02
Sgray <phongnguyen6120@gmail.com>
2019-05-16 21:46:36 +0700

    Update commonFunction.h

--- LibraryManagement/commonFunction.h
+++ LibraryManagement/commonFunction.h
@@ -15,9 +15,23 @@ int numDaysOfMonth(int Month, int Year);
 
 bool isPossibleDay(int Day, int Month, int Year);
 
-int plusOneIntoAChar(char numch);
+void getUsername(char username[]);
 
-bool plusOneIntoAString(char numStr[]);
+void getBirthday(Day &birthday);
+
+void getNationalID(char nationID[]);
+
+void getSex(int &sex);
+
+void getStatus(bool &status);
+
+void getTypeAccount(Users &A);
+
+void getReaderID(char ID[]);
+
+int wantEdit();
+
+char *toStr(long n);
 
 Day getExpiredDay(Day orginDay);
 
@@ -34,24 +48,3 @@ void printfDay(Day day);
 char *getStringFrmUser(char *note);
 
 int getNumber(char *note);
-// --------------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------------
-// ------------------------------------Link List-----------------------------------------------
-// --------------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------------
-
-//======================================================
-// << Hàm khởi tạo danh sách và node
-LLNodeReader *Init();
-NodeReader *InitNode(Readers *data);
-NodeReader *InitNode();
-//======================================================
-	
-NodeReader *fAddAtTail(LLNodeReader *&ls, Readers *data); // Thêm vào cuối danh sách một struct >> Readers
-
-void printfLLNodeReader(LLNodeReader *lsReader);
-
-Readers *findReaderAtNumberic(LLNodeReader *lsReader, int numberic);
-
-//=============================================================================================
-//=============================================================================================
