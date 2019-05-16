752170b2ab77103127031893d0df1c1e221daaa5
Sgray <phongnguyen6120@gmail.com>
2019-05-16 20:26:03 +0700

    Create conioLinux.h

---
+++ LibraryManagement/conioLinux.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#if defined(_WIN32) || defined(_WIN64)
+	void enable_raw_mode();
+
+	void disable_raw_mode();
+#else
+	#include <sys/ioctl.h>
+	#include <termios.h>
+	#include <stdio.h>
+
+	void enable_raw_mode();
+
+	void disable_raw_mode();
+
+	bool kbhit();
+
+
+	static struct termios old;
+	static struct termios news;
+
+	/* Initialize new terminal i/o settings */
+	void initTermios(int echo);
+
+	/* Restore old terminal i/o settings */
+	void resetTermios(void;
+
+	/* Read 1 character - echo defines echo mode */
+	char getch_(int echo);
+
+	/* Read 1 character without echo */
+	char getch(void);
+
+	/* Read 1 character with echo */
+	char getche(void);
+#endif
+// enable_raw_mode();
+// if (kbhit())
+// {
+// 	 code 
+// }
+// disable_raw_mode();
+// tcflush(0, TCIFLUSH); // Clear stdin to prevent characters appearing on prompt
