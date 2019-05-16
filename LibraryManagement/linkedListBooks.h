f2668fcac7e31fa61fcb5e2555cc7023ed6013ab
Sgray <phongnguyen6120@gmail.com>
2019-05-16 21:45:21 +0700

    Create linkedListBooks.h

---
+++ LibraryManagement/linkedListBooks.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "LibraryManagement.h"
+
+//======================================================
+// << Hàm khởi tạo danh sách và node
+
+void Init(LLNodeBook &ls);
+
+NodeBook *makeNode(Books data);
+//======================================================
+
+NodeBook* addAtTail(LLNodeBook &ls, Books data); // Thêm vào cuối danh sách một struct >> Books
+
+void freeLinkListBook(LLNodeBook &ls);
+
+//=============================================================================================
+//=============================================================================================
+
