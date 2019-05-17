6196f7928d17f1b2a128016d849ee2aa049b429d
Sgray <phongnguyen6120@gmail.com>
2019-05-17 23:36:10 +0700

    Create returnedBooks.h

---
+++ LibraryManagement/returnedBooks.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "LibraryManagement.h"
+#include "commonFunction.h"
+#include "readerManagement.h"
+#include "bookManagement.h"
+
+Day returnBookExpectDay(Day borrowBookDay); // trả về ngày trả sách dự kiến (7 ngày kể từ ngày mượn)
+
+bool possibleReader(Day expiredDay); // Thẻ độc giả còn hạn hay không? - 1 còn hạn; 0 là hết hạn
+
+void updateBookFile(Books book);
+
+void borrowBookBill(char *ID, char *Fullname);
+
+void borrowBook();
+
