05d7f13787b4e534240eb1bbee424ef4db6884f0
Sgray <phongnguyen6120@gmail.com>
2019-05-17 14:49:42 +0700

    Update functionsFile.cpp

--- LibraryManagement/functionsFile.cpp
+++ LibraryManagement/functionsFile.cpp
@@ -1,11 +1,10 @@
 #include "functionsFile.h"
-#define _DIR_DATA_FOLDER "Release/Database/"
 
 
-FILE *openFile(char *nameFile, char *mode){
+FILE *openFile(char* dir, char *nameFile, char *mode){
 
-	char *dirFile = new char(strlen((char*)_DIR_DATA_FOLDER) + 10);
-	strcat(dirFile,_DIR_DATA_FOLDER);
+	char *dirFile = new char(strlen(dir) + 20);
+	strcat(dirFile,dir);
 	strcat(dirFile, nameFile);
 
 	FILE *ptrFile = fopen(dirFile, mode);
