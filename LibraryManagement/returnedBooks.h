784bc973aff7d93074d32cff9803c426a58b9c15
Sgray <phongnguyen6120@gmail.com>
2019-05-18 17:33:18 +0700

    Update returnedBooks.h

--- LibraryManagement/returnedBooks.h
+++ LibraryManagement/returnedBooks.h
@@ -4,13 +4,13 @@
 #include "readerManagement.h"
 #include "bookManagement.h"
 
-Day returnBookExpectDay(Day borrowBookDay); // trả về ngày trả sách dự kiến (7 ngày kể từ ngày mượn)
+Day returnBookExpectDay(Day returnBookDay); // trả về ngày trả sách dự kiến (7 ngày kể từ ngày mượn)
 
 bool possibleReader(Day expiredDay); // Thẻ độc giả còn hạn hay không? - 1 còn hạn; 0 là hết hạn
 
 void updateBookFile(Books book);
 
-void borrowBookBill(char *ID, char *Fullname);
+void returnBookBill(char *ID, char *Fullname);
 
-void borrowBook();
+void returnBook();
 
