1944e397eedff6d9d84a28492e62398f051c8853
Sgray <phongnguyen6120@gmail.com>
2019-05-19 21:56:57 +0700

    Update analyzingLibrary.cpp

---
+++ LibraryManagement/analyzingLibrary.cpp
@@ -0,0 +1,13 @@
+#include "analyzingLibrary.h"
+
+
+// printf("1. Thống kê số lượng sách trong thư viện.\n");
+// printf("2. Thống kê số lượng sách theo thể loại.\n");
+// printf("3. Thống kê số lượng độc giả.\n");
+// printf("4. Thống kê số lượng độc giả theo giới tính.\n");
+// printf("5. Thống kê số sách đang được mượn.\n");
+// printf("6. Thống kê danh sách độc giả bị trễ hạn.\n");
+
+void runningAnalyzing(){
+
+}
