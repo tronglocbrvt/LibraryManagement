bbc97c23b8bbae22a1f587cfc369e53b6fe39754
Sgray <phongnguyen6120@gmail.com>
2019-05-19 21:57:04 +0700

    Update Menu.cpp

--- LibraryManagement/Menu.cpp
+++ LibraryManagement/Menu.cpp
@@ -156,3 +156,31 @@ int askYesNoQuestion(char *note){
 
 	return 2;
 }
+
+int analyzingMenu(){
+	system(cls);
+	printf("1. Thống kê số lượng sách trong thư viện.\n");
+	printf("2. Thống kê số lượng sách theo thể loại.\n");
+	printf("3. Thống kê số lượng độc giả.\n");
+	printf("4. Thống kê số lượng độc giả theo giới tính.\n");
+	printf("5. Thống kê số sách đang được mượn.\n");
+	printf("6. Thống kê danh sách độc giả bị trễ hạn.\n");
+	printf("0. Quay ve.\n");
+
+	return 6;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
