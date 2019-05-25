d9c64879ba64c15c78c6f046d4123e59a9d5da23
Sgray <phongnguyen6120@gmail.com>
2019-05-25 11:07:11 +0700

    cập nhật lại địa chỉ đến file

--- LibraryManagement/functionsFile.h
+++ LibraryManagement/functionsFile.h
@@ -24,18 +24,18 @@
 
 #else
 
-	#define _DIR_DATA_FOLDER_READER "Documents/Github/LibraryManagement/LibraryManagement/Release/Reader/databaseReader.bin"
-	#define _DIR_DATA_FOLDER_READER_TEMP "Documents/Github/LibraryManagement/LibraryManagement/Release/Reader/readerTemp.bin"
+	#define _DIR_DATA_FOLDER_READER "Documents/Github/LibraryManagement/LibraryManagement/Database/Reader/databaseReader.bin"
+	#define _DIR_DATA_FOLDER_READER_TEMP "Documents/Github/LibraryManagement/LibraryManagement/Database/Reader/readerTemp.bin"
 
-	#define _DIR_DATA_FOLDER_USER_CUR "Documents/Github/LibraryManagement/LibraryManagement/Release/Current/currentUser.bin"
-	#define _DIR_DATA_FOLDER_USER_TEMP "Documents/Github/LibraryManagement/LibraryManagement/Release/Users/userTemp.bin"
-	#define _DIR_DATA_FOLDER_USER "Documents/Github/LibraryManagement/LibraryManagement/Release/Users/Users.bin"
+	#define _DIR_DATA_FOLDER_USER_CUR "Documents/Github/LibraryManagement/LibraryManagement/Database/Current/currentUser.bin"
+	#define _DIR_DATA_FOLDER_USER_TEMP "Documents/Github/LibraryManagement/LibraryManagement/Database/Users/userTemp.bin"
+	#define _DIR_DATA_FOLDER_USER "Documents/Github/LibraryManagement/LibraryManagement/Database/Users/Users.bin"
 
-	#define _DIR_DATA_FOLDER_BOOK "Documents/Github/LibraryManagement/LibraryManagement/Release/Book/databaseBook.bin"
-	#define _DIR_DATA_FOLDER_BOOK_TEMP "Documents/Github/LibraryManagement/LibraryManagement/Release/Book/bookTemp.bin"
+	#define _DIR_DATA_FOLDER_BOOK "Documents/Github/LibraryManagement/LibraryManagement/Database/Book/databaseBook.bin"
+	#define _DIR_DATA_FOLDER_BOOK_TEMP "Documents/Github/LibraryManagement/LibraryManagement/Database/Book/bookTemp.bin"
 
-	#define _DIR_DATA_FOLDER_BOOK_BORROW "Documents/Github/LibraryManagement/LibraryManagement/Release/BorrowBook/databaseBorrowBook.bin"
-	#define _DIR_DATA_FOLDER_BOOK_BORROW_TEMP "Documents/Github/LibraryManagement/LibraryManagement/Release/BorrowBook/databaseBorrowBookTemp.bin"
+	#define _DIR_DATA_FOLDER_BOOK_BORROW "Documents/Github/LibraryManagement/LibraryManagement/Database/BorrowBook/databaseBorrowBook.bin"
+	#define _DIR_DATA_FOLDER_BOOK_BORROW_TEMP "Documents/Github/LibraryManagement/LibraryManagement/Database/BorrowBook/databaseBorrowBookTemp.bin"
 
 #endif
 
